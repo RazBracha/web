d5b9574aba728f4dae46b6171ef063f3fb2077cd
RazBracha <razi.bracha@gmail.com>
2023-11-25 12:02:26 +0200

    start

---
+++ myapp/src/Header.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import './Header.css'
+
+function Header() {
+
+  return (
+    <>
+      <h1 className="mainHeader animated fadeInDown">
+        Hi. I`m Raz. <br />
+        A Developer.
+      </h1>
+
+      <h2 className='subHeader'>I am passionate about technology and development.<br/>
+              Driven by the thrill of coding,  
+              and exploring emerging tech`s potential.</h2>
+
+
+    </>
+  )
+}
+
+export default Header
